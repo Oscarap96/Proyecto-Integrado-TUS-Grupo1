c3b9bb62a25e746fbbf1997f10b560a0f7cf0732
Lamberto <lamberto.ripoll@alumnos.unican.es>
2017-10-24 22:02:56 +0200

    Creadas las posibles clases necesarias para la interfaz. Añadido estilo de cada item de la lista de paradas.

---
+++ Src/TUS_Santander/app/src/main/java/es/unican/grupo1/tus_santander/Views/ListParadasAdapter.java
@@ -0,0 +1,8 @@
+package es.unican.grupo1.tus_santander.Views;
+
+/**
+ * Created by lamberto on 24/10/17.
+ */
+
+public class ListParadasAdapter {
+}
---
+++ Src/TUS_Santander/app/src/main/java/es/unican/grupo1/tus_santander/Views/ParadasFragment.java
@@ -0,0 +1,15 @@
+package es.unican.grupo1.tus_santander.Views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import es.unican.grupo1.tus_santander.R;
+
+public class ParadasFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_paradas_list);
+    }
+}
