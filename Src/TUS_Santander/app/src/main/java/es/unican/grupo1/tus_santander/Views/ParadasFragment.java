474a16e6d1de2ddd5fae173cb07c0231b310d5e8
Lamberto <lamberto.ripoll@alumnos.unican.es>
2017-10-25 17:21:22 +0200

    Interfaz para listar paradas implementada con fragments, similar a listar líneas. También he creado las correspondientes clases de estas interfaces pero vacías.

--- Src/TUS_Santander/app/src/main/java/es/unican/grupo1/tus_santander/Views/ParadasFragment.java
+++ Src/TUS_Santander/app/src/main/java/es/unican/grupo1/tus_santander/Views/ParadasFragment.java
@@ -1,15 +1,8 @@
 package es.unican.grupo1.tus_santander.Views;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.v4.app.ListFragment;
 
-import es.unican.grupo1.tus_santander.R;
+public class ParadasFragment extends ListFragment {
 
-public class ParadasFragment extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_paradas_list);
-    }
 }
