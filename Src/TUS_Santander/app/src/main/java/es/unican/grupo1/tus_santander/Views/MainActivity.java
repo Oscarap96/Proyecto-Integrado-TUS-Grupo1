ee22198b31518f7055274dadad3b7c899ddeccd9
Lamberto <lamberto.ripoll@alumnos.unican.es>
2017-11-11 18:05:26 +0100

    Borradas las clases EstimacionesActivity y ListaParadasActivity porque en realidad no hacían nada.

--- Src/TUS_Santander/app/src/main/java/es/unican/grupo1/tus_santander/Views/MainActivity.java
+++ Src/TUS_Santander/app/src/main/java/es/unican/grupo1/tus_santander/Views/MainActivity.java
@@ -1,6 +1,5 @@
 package es.unican.grupo1.tus_santander.Views;
 
-
 import android.os.Bundle;
 
 import android.support.v4.app.FragmentManager;
@@ -8,7 +7,6 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-
 
 import es.unican.grupo1.tus_santander.R;
 
@@ -21,49 +19,6 @@ public class MainActivity extends AppCompatActivity implements DataCommunication
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //TUSSQLiteHelper tusdbh = new TUSSQLiteHelper(this, "DBTUS", null, 1);
-
-        //SQLiteDatabase db = tusdbh.getWritableDatabase();
-
-        //this.deleteDatabase("DBTUS");
-
-        //Si hemos abierto correctamente la base de datos
-        /**if(db != null)
-        {
-            //Insertamos 5 lineas y paradas de ejemplo
-            for(int i=1; i<=5; i++)
-            {
-                Log.d("Entraaaaaa","Entra inserción de datos"+i);
-                ContentValues valores = new ContentValues();
-
-                //Generamos los datos
-                int codigo = i;
-                String nombre = "Linea" + i;
-                String numero = "Numero" + i;
-                int identificador = i;
-
-                int codParada = i;
-                String nombreP = "Parada" + i;
-                int identificadorP = i;
-
-                //valores.put("idLinea", codigo);
-                //valores.put("nombre", nombre);
-                //valores.put("numero", numero);
-                //valores.put("identificador", identificador);
-                //db.insert("Linea", null, valores);
-                //db.close();
-
-                //Insertamos los datos en la tabla Linea
-               db.execSQL("INSERT INTO Linea (idLinea, nombre, numero, identificador) " +
-                        "VALUES (" + codigo + ", '" + nombre +"', '" + numero +"', " + identificador + ")");
-
-
-                db.execSQL("INSERT INTO Parada (idParada, nombre, identificador) " +
-                        "VALUES (" + codParada + ", '" + nombreP +"', " + identificadorP + ")");
-            }
-            //Cerramos la base de datos
-            db.close();
-        }*/
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         LineasFragment fragmentLineas = new LineasFragment();
@@ -72,7 +27,6 @@ public class MainActivity extends AppCompatActivity implements DataCommunication
         ft.add(R.id.frameLayoutElements, fragmentLineas);
         ft.commit();
     }//onCreate
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
