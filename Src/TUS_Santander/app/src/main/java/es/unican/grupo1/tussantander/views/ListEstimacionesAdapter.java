1093c50dfcf0699382e579ab1e486dced2381fc8
pma93 <pma1193@gmail.com>
2017-11-22 08:35:48 +0100

    Cambiado el color de las líneas en la interfaz estimaciones para adecuarlo al oficial de TUS

--- Src/TUS_Santander/app/src/main/java/es/unican/grupo1/tussantander/views/ListEstimacionesAdapter.java
+++ Src/TUS_Santander/app/src/main/java/es/unican/grupo1/tussantander/views/ListEstimacionesAdapter.java
@@ -1,6 +1,7 @@
 package es.unican.grupo1.tussantander.views;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -39,11 +40,106 @@ public class ListEstimacionesAdapter extends ArrayAdapter {
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View viewRow = layoutInflater.inflate(R.layout.custom_list_estimaciones_layout, null, true);
+        TextView campoLinea = viewRow.findViewById(R.id.textView_linea);
         TextView linea = viewRow.findViewById(R.id.textView_nombreLinea);
         TextView tiempo1 = viewRow.findViewById(R.id.textView_tiempoBus1);
         TextView tiempo2 = viewRow.findViewById(R.id.textView_tiempoBus2);
         linea.setText(String.valueOf(estimaciones.get(position).getEtiquetaLinea().trim()));
         tiempo1.setText(String.valueOf(estimaciones.get(position).getTiempo1min()));
+        //Con esto nos aseguramos de que el texto (p.e. Línea 1)
+        //aparece con el color oficial de TUS.
+        switch (estimaciones.get(position).getEtiquetaLinea()){
+            case "1":
+                linea.setTextColor(Color.parseColor("#FD000A"));
+                campoLinea.setTextColor(Color.parseColor("#FD000A"));
+                break;
+            case "2":
+                linea.setTextColor(Color.parseColor("#A903CE"));
+                campoLinea.setTextColor(Color.parseColor("#A903CE"));
+                break;
+            case "3":
+                linea.setTextColor(Color.parseColor("#FFD21D"));
+                campoLinea.setTextColor(Color.parseColor("#FFD21D"));
+                break;
+            case "4":
+                linea.setTextColor(Color.parseColor("#21C4E3"));
+                campoLinea.setTextColor(Color.parseColor("#21C4E3"));
+                break;
+            case "5C1":
+                linea.setTextColor(Color.parseColor("#929292"));
+                campoLinea.setTextColor(Color.parseColor("#929292"));
+                break;
+            case "5C2":
+                linea.setTextColor(Color.parseColor("#929292"));
+                campoLinea.setTextColor(Color.parseColor("#929292"));
+                break;
+            case "6C1":
+                linea.setTextColor(Color.parseColor("#157A3A"));
+                campoLinea.setTextColor(Color.parseColor("#157A3A"));
+                break;
+            case "6C2":
+                linea.setTextColor(Color.parseColor("#157A3A"));
+                campoLinea.setTextColor(Color.parseColor("#157A3A"));
+                break;
+            case "7C1":
+                linea.setTextColor(Color.parseColor("#F26319"));
+                campoLinea.setTextColor(Color.parseColor("#F26319"));
+                break;
+            case "7C2":
+                linea.setTextColor(Color.parseColor("#F26319"));
+                campoLinea.setTextColor(Color.parseColor("#F26319"));
+                break;
+            case "11":
+                linea.setTextColor(Color.parseColor("#070F47"));
+                campoLinea.setTextColor(Color.parseColor("#070F47"));
+                break;
+            case "12":
+                linea.setTextColor(Color.parseColor("#9ECA6B"));
+                campoLinea.setTextColor(Color.parseColor("#9ECA6B"));
+                break;
+            case "13":
+                linea.setTextColor(Color.parseColor("#9083A9"));
+                campoLinea.setTextColor(Color.parseColor("#9083A9"));
+                break;
+            case "14":
+                linea.setTextColor(Color.parseColor("#0F69AB"));
+                campoLinea.setTextColor(Color.parseColor("#0F69AB"));
+                break;
+            case "15":
+                linea.setTextColor(Color.parseColor("#FFD61B"));
+                campoLinea.setTextColor(Color.parseColor("#FFD61B"));
+                break;
+            case "16":
+                linea.setTextColor(Color.parseColor("#681433"));
+                campoLinea.setTextColor(Color.parseColor("#681433"));
+                break;
+            case "17":
+                linea.setTextColor(Color.parseColor("#F37E80"));
+                campoLinea.setTextColor(Color.parseColor("#F37E80"));
+                break;
+            case "18":
+                linea.setTextColor(Color.parseColor("#B4E5D9"));
+                campoLinea.setTextColor(Color.parseColor("#B4E5D9"));
+                break;
+            case "19":
+                linea.setTextColor(Color.parseColor("#01848D"));
+                campoLinea.setTextColor(Color.parseColor("#01848D"));
+                break;
+            case "20":
+                linea.setTextColor(Color.parseColor("#73F4A7"));
+                campoLinea.setTextColor(Color.parseColor("#73F4A7"));
+                break;
+            case "21":
+                linea.setTextColor(Color.parseColor("#9DCD60"));
+                campoLinea.setTextColor(Color.parseColor("#9DCD60"));
+                break;
+            case "23":
+                linea.setTextColor(Color.parseColor("#C8C8C8"));
+                campoLinea.setTextColor(Color.parseColor("#C8C8C8"));
+                break;
+            default:
+                break;
+        }
         // Oculta la segunda linea en caso de que no tenga valor (-1)
         if (estimaciones.get(position).getTiempo2min() == -1) {
             viewRow.findViewById(R.id.linearLayout_terceraFila).setVisibility(View.GONE);
