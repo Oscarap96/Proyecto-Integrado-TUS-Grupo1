bd37fad5223377eeb7c275a8fdc52484c60a8777
Lamberto <lamberto.ripoll@alumnos.unican.es>
2017-11-23 09:39:36 +0100

    Quitado el criterio excepcional de id buscado. Simplificado y optimizado el código de búsqueda.

--- Src/TUS_Santander/app/src/main/java/es/unican/grupo1/tussantander/model/Parada.java
+++ Src/TUS_Santander/app/src/main/java/es/unican/grupo1/tussantander/model/Parada.java
@@ -103,15 +103,16 @@ public class Parada {
      * @return lista de resultados
      */
     public static List<Parada> buscarParada(List<Parada> paradas, String busqueda) {
-        String texto = busqueda.toUpperCase();
         // al comprobar nombre tendre que quitar los acentos tanto de la parada como de la busqueda
-        texto = quitarSimbolosInnecesarios(texto);
-        texto = quitarAcentos(texto);
-        List<Parada> resultado = new ArrayList<>();
+        // procesar el texto de busqueda para quitarle detalles que dificulten la busqueda
+        String textoBuscado = busqueda.toUpperCase();
+        textoBuscado = quitarSimbolosInnecesarios(textoBuscado);
+        textoBuscado = quitarAcentos(textoBuscado);
         // si la parada es un resultado de busqueda, lo anhade como resultado
-        for (int i = 0; i < paradas.size(); i++) {
-            if (comprobarResultadoParada(paradas.get(i), texto)) {
-                resultado.add(paradas.get(i));
+        List<Parada> resultado = new ArrayList<>();
+        for (Parada parada : paradas) {
+            if (comprobarResultadoParada(parada, textoBuscado)) {
+                resultado.add(parada);
             }
         }
         return resultado;
@@ -119,73 +120,27 @@ public class Parada {
 
     /**
      * Dada una parada y un texto de busqueda, comprueba si la parada seria un resultado a esa
-     * busqueda.
+     * busqueda. Comprueba el criterio de buscar tanto en el campo de id como en el nombre de la
+     * parada de manera unificada y ademas, que la parada contenga todos las palabras buscadas.
      *
      * @param parada   parada a comprobar si satisface la busqueda
      * @param busqueda texto buscado
      * @return true en caso de que parada sea un resultado de busqueda
      */
     private static boolean comprobarResultadoParada(Parada parada, String busqueda) {
-        boolean resultado = false;
-        // TODO
-        // comprobar cada uno de los criterios
-        if (criterioIdBuscado(parada, busqueda)) {
-            resultado = true;
-        } else if (criterioBusquedaUnificada(parada, busqueda)) {
-            resultado = true;
-        }
-        return resultado;
-    }
-
-    /**
-     * Verifica el criterio (o test de aceptacion) de "Resultados de paradas con id buscado"
-     *
-     * @param parada   parada a comprobar si es resultado
-     * @param busqueda texto buscado
-     * @return true en caso de que el identificador de la parada aparezca en algun lugar del texto
-     * de busqueda, false en caso contrario
-     */
-    private static boolean criterioIdBuscado(Parada parada, String busqueda) {
-        boolean resultado = false;
-        int identificador = parada.getIdentificador();
+        boolean resultado = true;
+        // obtener texto a comprobar de la parada
+        String textoParada = parada.getNombre() + " " + Integer.toString(parada.getIdentificador());
+        textoParada = quitarAcentos(textoParada);
+        // obtener palabras de busqueda
+        // comprobar que todas las palabras de busqueda estan contenidas en el texto de la parada
         for (String palabra : busqueda.split(" ")) {
-            try {
-                if (Integer.parseInt(palabra) == identificador) {
-                    resultado = true;
-                    break;
-                }
-            } catch (NumberFormatException e) {
+            if (!textoParada.contains(palabra)) {
+                resultado = false;
+                break;
             }
         }
         return resultado;
-    }
-
-    /**
-     * Obtiene la lista de identificadores que aparecen en el texto de busqueda.
-     *
-     * @param busqueda texto de busqueda
-     * @return lista de identificadores
-     */
-    private static int[] obtenerIdentificadoresBuscados(String busqueda) {
-        // TODO
-        // StringBuilder builder = new StringBuilder(busqueda);
-        // BufferedReader bufferedReader = new BufferedReader(busqueda);
-        // StringReader stringReader = new StringReader(busqueda);
-        // BufferedReader bufferedReader = new BufferedReader(stringReader);
-        // busqueda.split(" ");
-        ArrayList<Integer> listaIdentificadores = new ArrayList<>();
-        for (String palabra : busqueda.split(" ")) {
-            try {
-                listaIdentificadores.add(Integer.parseInt(palabra));
-            } catch (NumberFormatException e) {
-                // entra aqui si ha intentado castear otra cosa que no sea un numero
-            }
-        }
-        int[] identificadores = new int[listaIdentificadores.size()];
-        for (int i = 0; i < identificadores.length; i++) {
-            identificadores[i] = listaIdentificadores.get(i);
-        }
-        return identificadores;
     }
 
     /**
@@ -215,26 +170,6 @@ public class Parada {
         no quitar los puntos porque en ese caso no tienen que salir resultados, seria culpa del
         usuario, no hay razon para que ponga un punto sin querer
          */
-        String resultado = busqueda.replace("-", "");
-        // TODO pensar en mas cosas que sea razonable que se equivoque
-        return resultado;
-    }
-
-    // TODO
-    private static boolean criterioBusquedaUnificada(Parada parada, String busqueda) {
-        boolean resultado = true;
-        // obtener texto a comprobar de la parada
-        String textoParada = parada.getNombre() + " " + Integer.toString(parada.getIdentificador());
-        textoParada = quitarAcentos(textoParada);
-        // obtener palabras de busqueda
-        // comprobar que todas las palabras de busqueda estan contenidas en el texto de la parada
-        for (String palabra :busqueda.split(" ")) {
-            if (!textoParada.contains(palabra)) {
-                resultado = false;
-                break;
-            }
-        }
-        return resultado;
-        // TODO mirar si al buscar numeros, si pongo 3 tiene que salir como resultado 328
+        return busqueda.replace("-", "");
     }
 }
