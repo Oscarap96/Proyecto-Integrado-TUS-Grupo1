36946bf137e79411d660bbc240aeb29d3fe7a00f
Oscar Alario <oap00@alumnos.unican.es>
2017-11-27 13:28:22 +0100

    Eliminada la clase vacía 'MenuActionBar' y documentadas las clases 'MainActivity' y 'TarifasFragment'

--- Src/TUS_Santander/app/src/main/java/es/unican/grupo1/tussantander/views/MenuActionBar.java
+++
@@ -1,8 +0,0 @@
-package es.unican.grupo1.tussantander.views;
-
-/**
- * Created by Oscar Alario Pelaz on 21/11/2017.
- */
-
-public class MenuActionBar {
-}
--- Src/TUS_Santander/app/src/main/java/es/unican/grupo1/tussantander/views/TarifasFragment.java
+++ Src/TUS_Santander/app/src/main/java/es/unican/grupo1/tussantander/views/TarifasFragment.java
@@ -1,17 +1,14 @@
 package es.unican.grupo1.tussantander.views;
 
 
-import android.app.ListFragment;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.TextView;
 
 import es.unican.grupo1.tussantander.R;
 
@@ -19,18 +16,21 @@ import es.unican.grupo1.tussantander.R;
  * Created by Oscar Alario Pelaz on 21/11/2017.
  */
 
+/**
+ * Fragment utilizado para mostrar las tarifas de los autobuses de TUS.
+ */
 public class TarifasFragment extends Fragment {
 
     /**
      * Método que crea el fragment que se utiliza para mostrar las tarifas
-     * @param inflater
-     * @param container
-     * @param savedInstanceState
+     * @param inflater LayoutInflater utilizado para las tarifas
+     * @param container ViewGroup utilizado para las tarifas
+     * @param savedInstanceState Bundle utilizado para las tarifas
      * @return view correspondiente a las tarifas
      */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.custom_list_tarifas, container, false);
+        View view = inflater.inflate(R.layout.custom_tarifas, container, false);
 
         //Se busca el botón "MasInfo" y se guarda en una variable
         Button info = (Button) view.findViewById(R.id.buttonMasInfo);
